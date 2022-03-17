c91ef24779fe6a13b81214eef97ac8f3134c8e8c
sollametr <kuba.andrey.jr@yandex.ru>
2022-03-17 22:36:21 +0300

    - init

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ def two_params(nickname, level, rating):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("POST", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
